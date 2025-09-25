0aa39d1a49e93175c9b4704f0dee82973de42f47
rich-27 <rmlewis14@gmail.com>
2025-09-25 12:30:49 +0100

    Re-add eslint globals
    Apparently 'js/recommended' also doesn't include builtin globals

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,16 @@ import { defineConfig } from 'eslint/config';
 import babelParser from '@babel/eslint-parser';
 import js from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier/flat';
+import globals from "globals";
 
 export default defineConfig([
   {
     languageOptions: {
       parser: babelParser,
+      globals: {
+        ...globals.builtin,
+        ...globals.node,
+      }
     },
     files: ['**/*.js'],
     plugins: { js },
